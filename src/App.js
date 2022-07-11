bb05417f3e62fd0e2e26aa7fb5df1e5b02e903f8
harshaTejaR <40051202+harshaTejaR@users.noreply.github.com>
2022-07-12 04:48:01 +0530

    Created a face SVG

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { Container } from "@mui/system";
+import React from "react";
+import "./App.css";
+import { Face } from "./Components/Face";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi</h1>
+      <Container className="App" maxWidth = "inherit">
+        <h1>Hi Here We can place Header Laters</h1>
+        <div>
+          <Container>
+            <Face margin = "inherit"></Face>
+          </Container>
+        </div>
+      </Container>
     </div>
   );
 }
