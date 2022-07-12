e1a96c5d705cfebcb0937718e2b447c36eeffe1d
harshaTejaR <40051202+harshaTejaR@users.noreply.github.com>
2022-07-12 07:21:22 +0530

    Heading Changed To Features to be developed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
   return (
     <div className="App">
       <Container className="App" maxWidth="inherit">
-        <h1>Hi Here We can place Header Laters</h1>
+        <h3>Hi! This an App To visualize SVG Smileys made using React and D3.js</h3>
+        <h3>Features To be added like download SVG/PNG, sliders to change shapes and number of smilies</h3>
         <div>
           <FaceApp></FaceApp>
         </div>
